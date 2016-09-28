79aa856dfba24beee4e104bcbf99e17b58c01c48
nbkhope <ryumemaru@yahoo.com>
2016-09-28 11:44:19 -0700

    Implement basic signinUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
+
+const ROOT_URL = 'http://localhost:3090';
+
 export function signinUser({ email, password }) {
   // Submit email/pw to server
 
@@ -7,4 +13,20 @@ export function signinUser({ email, password }) {
   //   redirect to /feature Route
   // Otherwise:
   //   display an error to user
+
+  // With redux-thunk, you return a function that has access to dispatch
+  return (dispatch) => {
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then((response) => {
+        dispatch({ type: AUTH_USER });
+
+        localStorage.setItem('token', response.data.token);
+
+        // redirect to a different route programatically
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+
+      });
+  };
 }
