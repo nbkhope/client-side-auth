ecd5b5499c8d48faa9a3dd47db69b808cafac537
nbkhope <ryumemaru@yahoo.com>
2016-09-28 10:24:34 -0700

    Add axios to package dependencies

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export function signinUser({ email, password }) {
+  // Submit email/pw to server
+
+  // If all OK:
+  //   update state (authenticated: true)
+  //   save the token
+  //   redirect to /feature Route
+  // Otherwise:
+  //   display an error to user
+}
