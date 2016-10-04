1267dfe6006575a4f63b1c2e178dd5a2a14b8910
nbkhope <ryumemaru@yahoo.com>
2016-10-04 13:59:47 -0700

    Make local variable to hold the store and dspatch AUTH_USER action if there is a token stored in localStorage (so that if the user refreshes the page, he will still be logged in)

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import reduxThunk from 'redux-thunk';
 import App from './components/app';
 import requireAuth from './components/auth/require_auth';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
@@ -18,9 +19,19 @@ import Feature from './components/feature';
 import Welcome from './components/welcome';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+
+// If there is a token, consider the user to be signed in
+if (token) {
+  store.dispatch({
+    type: AUTH_USER
+  });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Welcome} />
