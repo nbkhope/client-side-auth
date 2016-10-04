b2d15d32d89bd3617647408398bc5811e7354d50
nbkhope <ryumemaru@yahoo.com>
2016-10-04 13:55:23 -0700

    Add Welcome component to be the page shown in the root route

---
+++ src/components/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h3>Welcome! Feel free to play around.</h3>
+
+      <p>{"React is pretty cool, isn't it?"}</p>
+    </div>
+  );
+}
+
+export default Welcome;
